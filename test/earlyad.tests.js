b942a3f53d8442f5e99d986b48d369cce115e368
Antonio Pérez Barrero <apbarrero@gmail.com>
2016-02-14 16:18:43 +0100

    Improve description of isNewerVersion test cases

--- test/earlyad.tests.js
+++ test/earlyad.tests.js
@@ -5,7 +5,7 @@ var assert = require('chai').assert;
 
 describe('Early adopter', function() {
 
-   it('should properly detect a version update', function() {
+   it('should return true if new version is greater than current', function() {
       assert.ok(isNewerVersion('1.2.3', '1.2.2'));
       assert.ok(isNewerVersion('1.4.1', '1.3.1'));
       assert.ok(isNewerVersion('2.3.1', '1.3.1'));
@@ -14,7 +14,7 @@ describe('Early adopter', function() {
       assert.ok(isNewerVersion('2.0.0', '1.8.10'));
       assert.ok(isNewerVersion('10.0.0', '9.2.18'));
    });
-   it('should return false if new version is older than current', function() {
+   it('should return false if new version is lesser than current', function() {
       assert.notOk(isNewerVersion('1.2.1', '1.2.2'));
       assert.notOk(isNewerVersion('1.4.1', '1.5.1'));
       assert.notOk(isNewerVersion('2.3.1', '3.3.1'));
