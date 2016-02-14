890329e2329d81cd57ad1bae2fc6cc7480476c1f
Antonio Pérez Barrero <apbarrero@gmail.com>
2016-02-14 13:17:50 +0100

    Detect version updates based on version strings

--- earlyad.js
+++ earlyad.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
 
+function parseVersion(v) {
+   var match = /(\d+)\.(\d+)\.(\d+)/.exec(v);
+   return {
+      major: parseInt(match[1], 10),
+      minor: parseInt(match[2], 10),
+      release: parseInt(match[3], 10)
+   };
+}
+
+function intVersion(v) {
+   return (v.major * 1000000) + (v.minor * 1000) + v.release;
+}
 // Checks if `newVersion` is actually greater than `curVersion`
 //
 // Both input parameters are expected to be test strings with
 // semantic version contents, i.e. `X.Y.Z`
 function isNewerVersion(newVersion, curVersion) {
-   return false;
+   var newV = parseVersion(newVersion);
+   var curV = parseVersion(curVersion);
+   return intVersion(newV) > intVersion(curV);
 }
 
 module.exports = isNewerVersion;
--- test/earlyad.tests.js
+++ test/earlyad.tests.js
@@ -9,6 +9,21 @@ describe('Early adopter', function() {
 
    it('should properly detect a version update', function() {
       assert.ok(isNewerVersion('1.2.3', '1.2.2'));
+      assert.ok(isNewerVersion('1.4.1', '1.3.1'));
+      assert.ok(isNewerVersion('2.3.1', '1.3.1'));
+      assert.ok(isNewerVersion('1.4.1', '1.3.12'));
+      assert.ok(isNewerVersion('1.15.0', '1.8.12'));
+      assert.ok(isNewerVersion('2.0.0', '1.8.10'));
+      assert.ok(isNewerVersion('10.0.0', '9.2.18'));
+   });
+   it('should return false if new version is older than current', function() {
+      assert.notOk(isNewerVersion('1.2.1', '1.2.2'));
+      assert.notOk(isNewerVersion('1.4.1', '1.5.1'));
+      assert.notOk(isNewerVersion('2.3.1', '3.3.1'));
+      assert.notOk(isNewerVersion('1.4.12', '1.5.1'));
+      assert.notOk(isNewerVersion('1.0.15', '1.2.1'));
+      assert.notOk(isNewerVersion('2.9.0', '3.0.10'));
+      assert.notOk(isNewerVersion('9.1.12', '10.0.1'));
    });
 });
 
