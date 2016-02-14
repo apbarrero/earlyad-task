518eba15e3e4b8ce5b819bf67c95a21d350a0a59
Antonio Pérez Barrero <apbarrero@gmail.com>
2016-02-14 15:53:43 +0100

    isNewerVersion deals with invalid parameter values

--- earlyad.js
+++ earlyad.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 function parseVersion(v) {
-   var match = /(\d+)\.(\d+)\.(\d+)/.exec(v);
+   var match = /^(\d+)\.(\d+)\.(\d+)$/.exec(v);
+   if (!match)
+      throw "Not semantic version compliant";
    return {
       major: parseInt(match[1], 10),
       minor: parseInt(match[2], 10),
@@ -12,14 +14,20 @@ function parseVersion(v) {
 function intVersion(v) {
    return (v.major * 1000000) + (v.minor * 1000) + v.release;
 }
+
 // Checks if `newVersion` is actually greater than `curVersion`
 //
 // Both input parameters are expected to be test strings with
 // semantic version contents, i.e. `X.Y.Z`
 function isNewerVersion(newVersion, curVersion) {
-   var newV = parseVersion(newVersion);
-   var curV = parseVersion(curVersion);
-   return intVersion(newV) > intVersion(curV);
+   try {
+      var newV = parseVersion(newVersion);
+      var curV = parseVersion(curVersion);
+      return intVersion(newV) > intVersion(curV);
+   }
+   catch (e) {
+      return false;
+   }
 }
 
 module.exports = isNewerVersion;
--- test/earlyad.tests.js
+++ test/earlyad.tests.js
@@ -23,5 +23,19 @@ describe('Early adopter', function() {
       assert.notOk(isNewerVersion('2.9.0', '3.0.10'));
       assert.notOk(isNewerVersion('9.1.12', '10.0.1'));
    });
+   it('should return false if any parameter is non-compliant with semantic version', function() {
+      assert.notOk(isNewerVersion('1.2.3', 'foo'));
+      assert.notOk(isNewerVersion('foo', '1.2.3'));
+      assert.notOk(isNewerVersion('foo', 'bar'));
+      assert.notOk(isNewerVersion('1.2.3', '1.0'));
+      assert.notOk(isNewerVersion('1.0', '1.2.3'));
+      assert.notOk(isNewerVersion('1.0', '1.0'));
+      assert.notOk(isNewerVersion('1.2.3', '42'));
+      assert.notOk(isNewerVersion('42', '1.2.3'));
+      assert.notOk(isNewerVersion('42', '42'));
+      assert.notOk(isNewerVersion('1.2.3', '1.2.3foo'));
+      assert.notOk(isNewerVersion('1.2.3foo', '1.2.3'));
+      assert.notOk(isNewerVersion('1.2.3foo', '1.2.3foo'));
+   });
 });
 
