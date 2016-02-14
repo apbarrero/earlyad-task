0fef7c59b62bdce79baf47a3d12c7077f855ef83
Antonio Pérez Barrero <apbarrero@gmail.com>
2016-02-14 21:42:01 +0100

    Implement isNewerVersion using node.js 'semver' package

--- earlyad.js
+++ earlyad.js
@@ -1,33 +1,17 @@
 #!/usr/bin/env node
 
-function parseVersion(v) {
-   var match = /^(\d+)\.(\d+)\.(\d+)$/.exec(v);
-   if (!match)
-      throw "Not semantic version compliant";
-   return {
-      major: parseInt(match[1], 10),
-      minor: parseInt(match[2], 10),
-      release: parseInt(match[3], 10)
-   };
-}
-
-function intVersion(v) {
-   return (v.major * 1000000) + (v.minor * 1000) + v.release;
-}
+var semver = require('semver');
 
 // Checks if `newVersion` is actually greater than `curVersion`
 //
-// Both input parameters are expected to be test strings with
-// semantic version contents, i.e. `X.Y.Z`
+// Both input parameters are expected to be text strings with
+// semantic version contents according to http://semver.org,
+// i.e. `X.Y.Z`
+// Will return false if any the parameter values are non-compliant
 function isNewerVersion(newVersion, curVersion) {
-   try {
-      var newV = parseVersion(newVersion);
-      var curV = parseVersion(curVersion);
-      return intVersion(newV) > intVersion(curV);
-   }
-   catch (e) {
-      return false;
-   }
+   return semver.valid(newVersion)
+       && semver.valid(curVersion)
+       && semver.gt(newVersion, curVersion);
 }
 
 // Looks for `repoUrl` to be included in `package.dependencies`
