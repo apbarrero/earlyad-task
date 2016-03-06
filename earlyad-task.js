b5633e29671c30906b76379dd00ee102a04710f4
Antonio Pérez Barrero <apbarrero@gmail.com>
2016-03-06 17:11:44 +0100

    EarlyAd task app accepting HTTP PUT requests using express

--- earlyad-task.js
+++ earlyad-task.js
@@ -1,73 +1,77 @@
-var EarlyAd = require('earlyad');
+var EarlyAd = require('earlyad').EarlyAd;
 var async = require('async');
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-   res.send('Hello World! This is Early Adopter');
+var app = express();
+app.use(bodyParser.json());
+
+app.put('/', function (req, res) {
+
+   var repolist = [
+      "apbarrero/earlyad-task",
+      "git://github.com/auth0/wt-cli.git",
+   ];
+
+   var webhook = req.body;
+   if (webhook.ref_type != 'tag') {
+      res.send("No tag update, nothing to do");
+   }
+   else {
+      var repo = webhook.repository.git_url;
+      var newVersion = webhook.ref;
+
+      var dep = new EarlyAd({ url: repo, version: newVersion }, { token: process.env.GITHUB_TOKEN });
+
+      var reposToUpdate = [];
+      async.series([
+         function(callback) {
+            dep.checkDepRepoList(repolist, function(err, res) {
+               if (err) callback(err);
+               else {
+                  if (res && res.length > 0) {
+                     reposToUpdate = res;
+                     var reposToUpdateNames = reposToUpdate.map(function(item, i, array) {
+                        return item.repo;
+                     });
+                     console.log("Number of repositories to update: " + reposToUpdate.length);
+                     console.log("Detected need to update " + JSON.stringify(reposToUpdateNames) + " dependent on " + dep.packageInfo.url);
+                  }
+                  else {
+                     console.log("No need to update " + repo + " on any repos in the list");
+                  }
+                  callback();
+               }
+            });
+         },
+         function(callback) {
+            async.map(reposToUpdate, function(r, callback) {
+               var data = {
+                  repoUrl: r.repo,
+                  pack: r.pack,
+                  title: "Update dependency on " + repo + " to new version " + newVersion
+               };
+
+               dep.createPullRequest(data, callback);
+            }, function(err, res) {
+               if (err) callback(err);
+               else {
+                  callback(null);
+               }
+            });
+         }
+      ], function(err, result) {
+         if (err) {
+            res.status(500);
+            res.send("Failed to process webhook. Error: " + err);
+         }
+         else {
+            res.send("Webhook successfully processed");
+         }
+      });
+
+   }
 });
 
 app.listen(5000);
-
-//module.exports = function (ctx, done) {
-
-   //var repolist = [
-      //"apbarrero/earlyad-task",
-      //"git://github.com/auth0/wt-cli.git",
-   //];
-
-   //var webhook = ctx.body;
-   //if (webhook.ref_type != 'tag') {
-      //done(null, "No tag update, nothing to do");
-   //}
-   //else {
-      //var repo = webhook.repository.git_url;
-      //var newVersion = webhook.ref;
-
-      //var dep = new EarlyAd({ url: repo, version: newVersion }, { token: ctx.data.GITHUB_TOKEN });
-
-      //var reposToUpdate = [];
-      //async.series([
-         //function(callback) {
-            //dep.checkDepRepoList(repolist, function(err, res) {
-               //if (err) callback(err);
-               //else {
-                  //if (res && res.length > 0) {
-                     //reposToUpdate = res;
-                     //var reposToUpdateNames = reposToUpdate.map(function(item, i, array) {
-                        //return item.repo;
-                     //});
-                     //console.log("Number of repositories to update: " + reposToUpdate.length);
-                     //console.log("Detected need to update " + JSON.stringify(reposToUpdateNames) + " dependent on " + dependency.url);
-                  //}
-                  //else {
-                     //console.log("No need to update " + repo + " on any repos in the list");
-                  //}
-                  //callback();
-               //}
-            //});
-         //},
-         //function(callback) {
-            //async.map(reposToUpdate, function(r, callback) {
-               //var data = {
-                  //repoUrl: r.repo,
-                  //pack: r.pack,
-                  //title: "Update dependency on " + repo + " to new version " + newVersion
-               //};
-
-               //dep.createPullRequest(data, callback);
-            //}, function(err, res) {
-               //if (err) callback(err);
-               //else {
-                  //callback(null);
-               //}
-            //});
-         //}
-      //], function(err, res) {
-         //if (err) done(err)
-         //else
-            //done(null, "Success");
-      //});
-   //}
-//};
 
